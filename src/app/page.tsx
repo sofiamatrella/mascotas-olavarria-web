45c448fae24095796740ec2a0d307ed4eead0406
Sofia <sofia.matrella@gmail.com>
2023-06-28 10:05:33 -0300

    Endpoint test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,30 @@ import styles from "./page.module.css";
 import AboutUs from "@/components/AboutUs";
 import FAQ from "@/components/FAQ";
 
-export default function Home() {
+const register = async () => {
+  const res = await fetch("http://localhost:3000/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: "sofia.matrella@gmail.com",
+      // username: "sofiamatrella",
+      password: "12345",
+      // firstName: "Sofía",
+      // lastName: "Matrella",
+      // profilePicture: "dafijdsf",
+    }),
+    cache: "no-store",
+  });
+
+  console.log(res);
+  const response = await res.json();
+  console.log("JSON", response);
+
+  // return res.json();
+};
+
+export default async function Home() {
+  const user = await register();
+
   return (
     <main className={styles.main}>
       <Landing />
