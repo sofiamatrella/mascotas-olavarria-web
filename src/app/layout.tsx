2f07fb5c817cdbf5e32d2aee35418552e6540060
Sofia <sofia.matrella@gmail.com>
2023-09-30 18:26:38 -0300

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { poppins, raleway } from "./fonts";
 import { LoggedProvider } from "@/context/LoggedContext";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Mascotas Olavarría",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="es" className={`${poppins.variable} ${raleway.variable}`}>
       <body className={raleway.className}>
         <LoggedProvider>{children}</LoggedProvider>
+        <Analytics />
       </body>
     </html>
   );
